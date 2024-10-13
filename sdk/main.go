1c5a2a3f0738e75a2190dee0c00b4192713a94db
sasha <nasdvoya@proton.me>
2024-10-13 22:02:30 +0100

    Using flag for basic cli

--- sdk/main.go
+++ sdk/main.go
@@ -1,10 +1,17 @@
-/*
-Copyright © 2024 nasdvoya nasdvoya@proton.me
-*/
 package main
 
-import "assetcommongo/sdk/cmd"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	cmd.Execute()
+	helpFlag := flag.Bool("help", false, "Returns configuration")
+	// runFlag := flag.Bool("run", false, "Runs the asset")
+
+	flag.Parse()
+
+	if *helpFlag {
+		fmt.Println("The asset help was requested")
+	}
 }
