34d9dafa4108ad30b45cb39962a430e4ed4f7a28
sasha <nasdvoya@proton.me>
2024-10-13 18:43:41 +0100

    Better example implementation

---
+++ examples/configuration/asset.go
@@ -0,0 +1,47 @@
+package configuration
+
+import "assetcommongo/sdk/configuration"
+
+type Tag struct {
+	configuration.TagBase
+	Foo int    `json:"foo"`
+	Bar string `json:"bar"`
+}
+
+type AssetBase struct {
+	MqttParameters configuration.MqttParameters `json:"MqttParameters"`
+	Tags           []Tag                        `json:"Tags"`
+}
+
+func ReturnExampleConfig() string {
+	data := `{
+		"mqttParameters": {
+			"address": "192.168.1.100",
+			"port": "1883",
+			"isAnonymous": false,
+			"username": "user123",
+			"password": "pass122"
+		},
+		"tags": [
+			{
+				"name": "Temperature",
+				"route": 0,
+				"publish": 1,
+				"mqttTopics": "sensors/temp",
+				"mqttRetain": true,
+				"foo": 42,
+				"bar": "somevalue"
+			},
+			{
+				"name": "Humidity",
+				"route": 1,
+				"publish": 2,
+				"mqttTopics": "sensors/humidity",
+				"mqttRetain": false,
+				"foo": 43,
+				"bar": "othervalue"
+			}
+		]
+	}`
+	return data
+}
--- examples/main.go
+++ examples/main.go
@@ -1,54 +1,16 @@
 package main
 
 import (
-	"assetcommongo/sdk/configuration"
+	"assetcommongo/examples/configuration"
 	"encoding/json"
 	"fmt"
 )
 
-type Tag struct {
-	configuration.TagBase
-	Foo int    `json:"foo"`
-	Bar string `json:"bar"`
-}
-
-type AssetBase struct {
-	MqttParameters configuration.MqttParameters `json:"mqttParameters"`
-	Tags           []Tag                        `json:"tags"`
-}
-
 func main() {
-	data := `{
-		"mqttParameters": {
-			"address": "192.168.1.100",
-			"port": "1883",
-			"isAnonymous": false,
-			"username": "user123",
-			"password": "pass122"
-		},
-		"tags": [
-			{
-				"name": "Temperature",
-				"route": 0,
-				"publish": 1,
-				"mqttTopics": "sensors/temp",
-				"mqttRetain": true,
-				"foo": 42,
-				"bar": "somevalue"
-			},
-			{
-				"name": "Humidity",
-				"route": 1,
-				"publish": 2,
-				"mqttTopics": "sensors/humidity",
-				"mqttRetain": false,
-				"foo": 43,
-				"bar": "othervalue"
-			}
-		]
-	}`
+	// TODO: Testing like this for now, need unit tests
+	data := configuration.ReturnExampleConfig()
 
-	var assetData AssetBase
+	var assetData configuration.AssetBase
 	err := json.Unmarshal([]byte(data), &assetData)
 	if err != nil {
 		fmt.Print("Error deserializing AssetBase JSON: ", err)
@@ -61,5 +23,4 @@ func main() {
 		return
 	}
 	fmt.Println(string(indentedJson))
-
 }
